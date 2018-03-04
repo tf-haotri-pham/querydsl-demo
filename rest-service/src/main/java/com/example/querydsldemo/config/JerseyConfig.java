4e98ac6647fe092a9a3e12b6fb03f722329276e0
Hao Tri Pham <HaoTri.Pham@twinformatics.at>
2018-03-04 10:42:15 +0100

    Basic Swagger setup

--- rest-service/src/main/java/com/example/querydsldemo/config/JerseyConfig.java
+++ rest-service/src/main/java/com/example/querydsldemo/config/JerseyConfig.java
@@ -2,11 +2,16 @@ package com.example.querydsldemo.config;
 
 import com.example.querydsldemo.RootResource;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.jaxrs.listing.ApiListingResource;
+import io.swagger.jaxrs.listing.SwaggerSerializers;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
@@ -19,6 +24,9 @@ public class JerseyConfig extends ResourceConfig {
 
     static final String JERSEY_BASE_PATH = "/api";
 
+    @Value("${spring.jersey.application-path:" + JERSEY_BASE_PATH + "}")
+    private String apiPath;
+
     public JerseyConfig() {
         //Jersey providers
         register(JerseyObjectMapperProvider.class);
@@ -28,6 +36,28 @@ public class JerseyConfig extends ResourceConfig {
         
         // Jersey REST endpoints
         register(RootResource.class);
+    }
+
+    @PostConstruct
+    public void init() {
+        configureSwaggerForJersey();
+    }
+
+    private void configureSwaggerForJersey() {
+        // Available at localhost:port/swagger.json
+        register(ApiListingResource.class);
+        register(SwaggerSerializers.class);
+
+        final BeanConfig config = new BeanConfig();
+        config.setConfigId("springboot-jersey-swagger-querydsl-demo");
+        config.setTitle("QueryDSL Demo Swagger Documentation");
+        config.setVersion("v1");
+        config.setContact("HaoTri.Pham@twinformatics.at");
+        config.setSchemes(new String[]{"http", "https"});
+        config.setBasePath(apiPath);
+        config.setResourcePackage(RootResource.class.getPackage().getName());
+        config.setPrettyPrint(true);
+        config.setScan(true);
     }
 
     /**
