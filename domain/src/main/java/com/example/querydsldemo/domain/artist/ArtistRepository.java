f5e8853bbdc6b985f82efe33965a496d0db83f33
Hao Tri Pham <HaoTri.Pham@twinformatics.at>
2018-03-04 12:29:42 +0100

    Artist REST interactions

---
+++ domain/src/main/java/com/example/querydsldemo/domain/artist/ArtistRepository.java
@@ -0,0 +1,9 @@
+package com.example.querydsldemo.domain.artist;
+
+import com.example.querydsldemo.domain.BaseEntityRepository;
+import com.example.querydsldemo.domain.artist.Artist;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArtistRepository extends BaseEntityRepository<Artist> {
+}
--- rest-service/src/main/java/com/example/querydsldemo/config/DatabaseConfig.java
+++ rest-service/src/main/java/com/example/querydsldemo/config/DatabaseConfig.java
@@ -1,9 +1,16 @@
 package com.example.querydsldemo.config;
 
+import com.example.querydsldemo.domain.BaseEntity;
+import com.example.querydsldemo.domain.BaseEntityRepository;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EntityScan(basePackageClasses = { BaseEntity.class, Jsr310JpaConverters.class })
+@EnableJpaRepositories(basePackageClasses = BaseEntityRepository.class)
 @EnableTransactionManagement
 public class DatabaseConfig {
 }
