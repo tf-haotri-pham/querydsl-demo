b55254bd24e461d2052eaed9716e43cdf3b92a5d
Hao Tri Pham <HaoTri.Pham@twinformatics.at>
2018-03-04 15:48:42 +0100

    Spring Data query

--- domain/src/main/java/com/example/querydsldemo/domain/film/FilmRepository.java
+++ domain/src/main/java/com/example/querydsldemo/domain/film/FilmRepository.java
@@ -1,8 +1,16 @@
 package com.example.querydsldemo.domain.film;
 
 import com.example.querydsldemo.domain.BaseEntityRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FilmRepository extends BaseEntityRepository<Film> {
+
+    @Query("select f from Film f where f.director.firstName like %:name% OR f.director.lastName like %:name%")
+    List<Film> findByDirectorName(@Param("name") String name);
+    
 }
--- rest-service/src/main/java/com/example/querydsldemo/film/FilmResource.java
+++ rest-service/src/main/java/com/example/querydsldemo/film/FilmResource.java
@@ -2,19 +2,31 @@ package com.example.querydsldemo.film;
 
 import com.example.querydsldemo.domain.film.Film;
 import com.example.querydsldemo.domain.film.FilmRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Singleton;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 @Component
 @Singleton
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@Api(tags = "Film")
 public class FilmResource {
 
     @Autowired
@@ -22,15 +34,25 @@ public class FilmResource {
 
     @GET
     @Path("/{filmId}")
+    @ApiOperation("retrieves one film")
     public Film retrieveFilm(@PathParam("filmId") final long filmId) {
         return Optional.ofNullable(filmRepository.findOne(filmId))
                 .orElseThrow(() -> new NotFoundException("No film for ID " + filmId));
     }
 
     @POST
+    @ApiOperation("creates a film")
     public Film createFilm(final Film film) {
         film.setId(null);
         return filmRepository.save(film);
+    }
+
+    @GET
+    @ApiOperation("searches for films by director")
+    public List<Film> searchFilmByDirectorName(@QueryParam("director-name") final String directorName) {
+        return StringUtils.isBlank(directorName)
+                ? Collections.emptyList()
+                : filmRepository.findByDirectorName(directorName);
     }
     
 }
