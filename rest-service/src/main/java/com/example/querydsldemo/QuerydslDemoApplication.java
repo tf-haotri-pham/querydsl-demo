458e6cffaffba73c95a3c5d8a95fce32c6cdb8f1
Hao Tri Pham <HaoTri.Pham@twinformatics.at>
2018-03-04 09:45:50 +0100

    Convert to maven multi-module project

--- rest-service/src/main/java/com/example/querydsldemo/QuerydslDemoApplication.java
+++ rest-service/src/main/java/com/example/querydsldemo/QuerydslDemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class QuerydslDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QuerydslDemoApplication.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(QuerydslDemoApplication.class, args);
+    }
 }
