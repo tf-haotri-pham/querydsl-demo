bfca5d75c377936e129734b0f46b9d020cd02e00
Hao Tri Pham <phamhaotri@googlemail.com>
2018-03-04 08:44:30 +0100

    SPRING INITIALIZR: JPA, Jersey, Flyway

---
+++ src/main/java/com/example/querydsldemo/QuerydslDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.querydsldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerydslDemoApplication.class, args);
+	}
+}
